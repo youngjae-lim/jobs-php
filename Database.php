374669fbbe6002b6954c812b41ac17c1474d79e3
Youngjae Lim <limyoungjae@gmail.com>
2023-12-16 22:01:14 -0500

    Housekeeping

--- Database.php
+++ Database.php
@@ -20,7 +20,6 @@ class Database
 
         try {
             $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
-            echo 'connected';
         } catch (PDOException $e) {
             throw new Exception("Database connection failed: {$e->getMessage()}");
         }
--- public/index.php
+++ public/index.php
@@ -8,11 +8,15 @@ $db = new Database($config);
 
 require basePath('Router.php');
 
+// Instantiate the routher
 $router = new Router();
 
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
