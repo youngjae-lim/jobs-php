4e0e275d80afdd6ba62245c38141cd48f9a514b9
Youngjae Lim <limyoungjae@gmail.com>
2023-12-16 21:49:04 -0500

    Add options to PDO

--- Database.php
+++ Database.php
@@ -15,10 +15,11 @@ class Database
 
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
 
         try {
-            $this->conn = new PDO($dsn, $config['username'], $config['password']);
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
             echo 'connected';
         } catch (PDOException $e) {
             throw new Exception("Database connection failed: {$e->getMessage()}");
