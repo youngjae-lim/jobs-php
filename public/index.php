db387da8af854a96f6b4aaf2164cb528f768cd3a
Youngjae Lim <limyoungjae@gmail.com>
2023-12-18 19:25:33 -0500

    Add composer and PSR-4 autoloader

--- public/index.php
+++ public/index.php
@@ -1,14 +1,15 @@
 <?php
 
 require '../helpers.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Autoload classes
-spl_autoload_register(function ($class) {
-    $path = basePath("Framework/{$class}.php");
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+// spl_autoload_register(function ($class) {
+//     $path = basePath("Framework/{$class}.php");
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 // Instantiate the routher
 $router = new Router();
