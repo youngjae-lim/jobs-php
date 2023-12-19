f4e4457608e47998d17f955cd720134e64969cdb
Youngjae Lim <limyoungjae@gmail.com>
2023-12-18 19:08:16 -0500

    Add custom autoloader

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 
 require '../helpers.php';
-require basePath('Framework/Database.php');
-require basePath('Framework/Router.php');
+
+// Autoload classes
+spl_autoload_register(function ($class) {
+    $path = basePath("Framework/{$class}.php");
+    if (file_exists($path)) {
+        require $path;
+    }
+});
 
 // Instantiate the routher
 $router = new Router();
