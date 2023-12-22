82a53a7f318c0a88d2cfe1272f27951013dd08a9
Youngjae Lim <limyoungjae@gmail.com>
2023-12-22 15:39:06 -0500

    Change /listing/{id} to /listings/{id} for consistency

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 
-$router->get('/', "HomeController@index");
+$router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
-$router->get('/listing/{id}', 'ListingController@show');
+$router->get('/listings/{id}', 'ListingController@show');
 $router->post('/listings', 'ListingController@store');
